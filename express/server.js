da09c7e786f6ebc97a339502577bb2d8152c3b88
Courtney <csquellati511@gmail.com>
2025-04-22 21:34:41 -0500

    Established a connection to the MongoDB successfully

--- express/server.js
+++ express/server.js
@@ -1,7 +1,10 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+
+const MONGO_URI = 'mongodb+srv://cjstr4:FinalProjectTeam03@mizzouwebdevbudgetbotpr.uhgxvse.mongodb.net/'
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/ping', function (req, res) {
@@ -11,5 +14,15 @@ app.get('/ping', function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch(err => {
+    console.error(' Something went wrong with the connection:', err);
+    process.exit(1);
+  });
 
 app.listen(process.env.PORT || 8080);
