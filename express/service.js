2a8cb8ed741e6a89bf3838cb8622c67fddf8bbb1
chwebb02 <chwebb2002@gmail.com>
2025-05-01 15:27:41 -0500

    Implemented log in and registration flows in endpoint and service layer.

--- express/service.js
+++ express/service.js
@@ -2,12 +2,29 @@ const db = require('./database');
 const bcrypt = require('bcrypt')
 
 // Implementation of service layer
-export async function register(username, password) {
+async function register(username, password) {
     const user = db.get_user_by_username(username)
     if (user != null) {
         throw new Error("Username already exists");
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    return await db.create_user(username, hashedPassword)
+    const result = await db.create_user(username, hashedPassword)
+    return result._id;
 }
+
+async function login(username, password) {
+    const user = db.get_user_by_username(username)
+    if (user === null) {
+        throw new Error("User does not exist");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    if (hashedPassword === user.password) {
+        return user._id;
+    }
+
+    throw new Error("Bad login");
+}
+
+module.exports = { register, login };
