7c4485b51aa29e75abe08b48ee792934bf147bd9
chwebb02 <chwebb2002@gmail.com>
2025-05-01 14:26:57 -0500

    Added missing argument to function call

--- express/service.js
+++ express/service.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt')
 
 // Implementation of service layer
 export async function register(username, password) {
-    const user = db.get_user_by_username()
+    const user = db.get_user_by_username(username)
     if (user != null) {
         throw new Error("Username already exists");
     }
