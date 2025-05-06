9cc16bbbb613caa7d97851f69e12345a72ff4725
Osthoff <fisk.dominique@gmail.com>
2025-05-05 20:57:31 -0500

    Rerouted Transaction and Budget pages to the login page if user isn't signed in.

--- src/pages/CreateTransaction.jsx
+++ src/pages/CreateTransaction.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import api, { API_ROUTES } from '../api';
 
 const CreateTransaction = () => {
   const navigate = useNavigate();
+
+    useEffect(() => {
+      if (sessionStorage.getItem("username") == null) {
+        navigate("/login");
+      }
+    }, [navigate]);
   
   const [formData, setFormData] = useState({
     description: '',
